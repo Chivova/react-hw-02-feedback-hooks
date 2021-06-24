82673b3efb7d184c8b6df4524f22234a0155b460
unknown <Chivovaz@ua.fm>
2021-06-24 22:46:21 +0300

    started hw

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import '/modern-normalize/modern-normalize.css';
 
 function App() {
+  // const [good, setGood] = useState('');
+  // const [neutral, setNeutral] = useState('');
+  // const [bad, setBad] = useState('');
+
+  // const handleChange = e => {
+  //   const { good, neutral, bad } = e.trget;
+  // };
   return <div className="App"></div>;
 }
 
